47b6cf5473d2a4019ba168c831714b825e401ce8
Jason Reilly <jdr0887@renci.org>
2013-12-19 13:30:06 -0500

    organizing imports

--- renci-exogeni-service/src/main/java/org/renci/gate/plugin/exogeni/ExoGENIGATEService.java
+++ renci-exogeni-service/src/main/java/org/renci/gate/plugin/exogeni/ExoGENIGATEService.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.renci.gate.AbstractGATEService;
 import org.renci.gate.GATEException;
 import org.renci.gate.GlideinMetric;
